99aa224870d276b713fab4e55baf435dc70fa2af
AlexplaysMario823 <144667986+AlexplaysMario823@users.noreply.github.com>
2024-03-02 16:19:11 +0000

    added sound to running.html

--- factorial.js
+++ factorial.js
@@ -2,7 +2,19 @@ console.log('Iterative', factorialIterative(8))
 console.log('Iterative', factorialRecursive(8))
 
 function factorialIterative (n) {
-    let k = n
+    let total = n
+    let next = n
+    while (true){
+        next -= 1
+        if (next > 1) {
+            total = total * next
+        }
+        else {
+            return total
+        }
+         
+
+    }
 
 }
 
